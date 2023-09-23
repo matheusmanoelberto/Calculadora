eb991281220640d37b7107bd9d6ae1a0eb177ebd
matheusmanoelberto <matheusmanoel51@gmail.com>
2023-09-23 18:55:35 -0300

    testando

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -1,15 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Calculadora.Models
+namespace Calculadora.Models;
+public class Calculadora
 {
-    public class Calculadora
+    public void Somar(int x, int y)
     {
-        public void Somar(int x, int y)
-        {
-            Console.WriteLine($"{x} + {y} = {x + y}")
-        }
+        Console.WriteLine($"{x} + {y} = {x + y}");
+    }
+    public void Subtrair(int x, int y)
+    {
+        Console.WriteLine($"{x} - {y} = {x - y}");
+    }
+    public void Miltiplicar(int x, int y)
+    {
+        Console.WriteLine($"{x} X {y} = {x * y}");
+    }
+    public void Dividir(int x, int y)
+    {
+        Console.WriteLine($"{x} / {y} = {x / y}");
     }
 }
